d48d22c62109795289101d8ce89867725ae28897
Mathias Schaller <stoertebecker@gmx.at>
2019-12-11 15:24:05 +0100

    Clean Start

---
+++ src/test/java/at/kamadesign/Lagerverwaltung/LagerverwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package at.kamadesign.Lagerverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagerverwaltungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
