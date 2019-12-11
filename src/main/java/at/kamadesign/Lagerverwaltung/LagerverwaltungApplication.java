908f8f69030030ecd8b415b96c443637aed949c6
Mathias Schaller <stoertebecker@gmx.at>
2019-12-11 17:12:19 +0100

    Regal erstellen

--- src/main/java/at/kamadesign/Lagerverwaltung/LagerverwaltungApplication.java
+++ src/main/java/at/kamadesign/Lagerverwaltung/LagerverwaltungApplication.java
@@ -1,5 +1,7 @@
 package at.kamadesign.Lagerverwaltung;
 
+import at.kamadesign.Lagerverwaltung.model.Regal;
+import at.kamadesign.Lagerverwaltung.model.Regalfach;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,21 @@ public class LagerverwaltungApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LagerverwaltungApplication.class, args);
-	}
 
+		Regal regal = new Regal(1,8,8);
+		Regal regal2 = new Regal(2,12,12);
+
+		for (Regalfach fach: regal.getRegal_faecher()) {
+			int reihe  = fach.getFach_regal_reihe();
+			int spalte  = fach.getFach_regal_spalte();
+			System.out.println("Reihe " + reihe + " Spalte " + spalte);
+		}
+
+		for (Regalfach fach: regal2.getRegal_faecher()) {
+			int reihe  = fach.getFach_regal_reihe();
+			int spalte  = fach.getFach_regal_spalte();
+			System.out.println("Reihe " + reihe + " Spalte " + spalte);
+		}
+
+	}
 }
--- src/main/java/at/kamadesign/Lagerverwaltung/model/Regal.java
+++ src/main/java/at/kamadesign/Lagerverwaltung/model/Regal.java
@@ -8,6 +8,54 @@ public class Regal {
     private int regal_spalten;
     private ArrayList<Regalfach> regal_faecher;
 
+    public Regal(int regal_id, int regal_reihen, int regal_spalten) {
+        this.regal_id = regal_id;
+        this.regal_reihen = regal_reihen;
+        this.regal_spalten = regal_spalten;
+        regal_faecher = new ArrayList<Regalfach>();
 
+        int faecher_anzahl = regal_reihen * regal_spalten;
+        int reihe_aktuell = 1;
+        int spalte_aktuell = 0;
 
+        for (int i = 0; i < regal_reihen; i++) {
+            while (spalte_aktuell < regal_spalten ) {
+                spalte_aktuell++;
+                regal_faecher.add(new Regalfach(reihe_aktuell, spalte_aktuell));
+            }
+            reihe_aktuell ++;
+            spalte_aktuell = 0;
+        }
+    }
+    public int getRegal_id() {
+        return regal_id;
+    }
+
+    public void setRegal_id(int regal_id) {
+        this.regal_id = regal_id;
+    }
+
+    public int getRegal_reihen() {
+        return regal_reihen;
+    }
+
+    public void setRegal_reihen(int regal_reihen) {
+        this.regal_reihen = regal_reihen;
+    }
+
+    public int getRegal_spalten() {
+        return regal_spalten;
+    }
+
+    public void setRegal_spalten(int regal_spalten) {
+        this.regal_spalten = regal_spalten;
+    }
+
+    public ArrayList<Regalfach> getRegal_faecher() {
+        return regal_faecher;
+    }
+
+    public void setRegal_faecher(ArrayList<Regalfach> regal_faecher) {
+        this.regal_faecher = regal_faecher;
+    }
 }
